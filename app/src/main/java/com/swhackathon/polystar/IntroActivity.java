5ba78dfcda7b3fc4e70eef87327d14d23a373810
chea yeon <cna3762@gmail.com>
2021-02-04 20:43:31 +0900

    메인화면디자인!

--- app/src/main/java/com/swhackathon/polystar/IntroActivity.java
+++ app/src/main/java/com/swhackathon/polystar/IntroActivity.java
@@ -1,14 +1,31 @@
 package com.swhackathon.polystar;
-
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageButton;
 public class IntroActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        //타이틀바 삭제 시작
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        //타이틀바 삭제 종료
+        ImageButton imageButton = (ImageButton) findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    protected  void onPause(){
+        super.onPause();
+        finish();
     }
 }
